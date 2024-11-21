122e9c0b3db728ebf829fb20e44c7184da26b390
Данил Шифман <danilshifman07@gmail.com>
2024-11-21 18:31:45 +0300

    Удаление файлов

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
+from django.db.models import Q
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'category__title'
+    ).filter(
+        Q(is_published=True) &
+        Q(is_on_main=True) &
+        Q(category__is_published=True)
+    ).order_by('title')[:3]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
