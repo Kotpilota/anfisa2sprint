96e0f8c48639dd09ec1b2743bfb335a9060e78b5
Данил Шифман <danilshifman07@gmail.com>
2024-11-26 20:44:07 +0300

    удаление старых шаблонов

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -1,5 +1,8 @@
+import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
+
+from anfisa_for_friends import settings
 
 urlpatterns = [
     path('', include('homepage.urls')),
@@ -7,3 +10,7 @@ urlpatterns = [
     path('ice_cream/', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import IceCream, Category, Topping, Wrapper
+
+empty_value_display = 'Не задано'
+
+@admin.register(IceCream)
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+class IceCreamInline(admin.TabularInline):
+    model = IceCream
+    extra = 0
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+       inlines = (
+           IceCreamInline,
+       )
+       list_display = (
+           'title',
+       )
+
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+admin.site.empty_value_display = 'Не задано'
